65ca3779e7f4c096293c2629ec5fdd60cf2b6661
webdb81 <webdb81@gmail.com>
2023-07-08 16:29:34 +0300

    Test with notResult

---
+++ __tests__/index.test.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import gendiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFilePath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
+
+test('compare test', () => {
+  const file1 = getFilePath('file1.json');
+  const file2 = getFilePath('file2.json');
+  const expectedFile = getFilePath('expected_result.txt');
+
+  const compareFiles = gendiff(file1, file2);
+  // const expectedResult = fs.readFileSync(expectedFile, { encoding: 'utf-8' });
+  const expectedResult = fs.readFileSync(expectedFile, 'utf-8');
+
+  // console.log(file1);
+  // console.log(file2);
+  console.log(`Received\n${compareFiles}`);
+  console.log(`Expected\n${expectedResult}`);
+
+  // expect(compareFiles).toBe(expectedResult);
+  expect(compareFiles).toEqual(expectedResult);
+});
--- src/comparer.js
+++ src/comparer.js
@@ -1,43 +1,27 @@
 import _ from 'lodash';
 
-/* v2
-const comparer = (dataFile1, dataFile2) => {
-  const keysFile1 = _.keys(dataFile1);
-  const keysFile2 = _.keys(dataFile2);
-  const allKeys = _.sortBy(_.union(keysFile1, keysFile2));
-
-  const result = allKeys.reduce((acc, key) => {
-    if (!_.has(dataFile1, key)) {
-      acc.push(`  + ${key}: ${dataFile2[key]}\n`);
-    } else if (!_.has(dataFile2, key)) {
-      acc.push(`  - ${key}: ${dataFile1[key]}\n`);
-    } else if (dataFile1[key] !== dataFile2[key]) {
-      acc.push(`  - ${key}: ${dataFile1[key]} \n  + ${key}: ${dataFile2[key]}\n`);
-    } else {
-      acc.push(`    ${key}: ${dataFile1[key]}\n`);
-    }
-    return acc;
-  }, []);
-  return `{\n${result.join('')}}`;
-};
+/* v1
 */
-
 const comparer = (dataFile1, dataFile2) => {
   const keys = _.union(Object.keys(dataFile1), Object.keys(dataFile2)).sort();
   // const keys = _.union(_.keys(dataFile1), _.keys(dataFile2)).sort();
 
-  // const getDiff = keys.map((key) => { ===>> two timeout on same line
   const getDiff = keys.flatMap((key) => {
     const tempResult = [];
 
-    // Keys Same, Values Same
-    // if (_.has(dataFile1, key) && _.has(dataFile2, key)) {
-    if (Object.hasOwn(dataFile1, key) && Object.hasOwn(dataFile2, key)) {
-      if (dataFile1[key] === dataFile2[key]) {
-        tempResult.push(`    ${key}: ${dataFile1[key]}`);
-      }
+    // Key in file2, not in file1 — ADDED
+    // if (!_.has(dataFile1, key) && _.has(dataFile2, key)) {
+    if (!Object.hasOwn(dataFile1, key) && Object.hasOwn(dataFile2, key)) {
+      tempResult.push(`  + ${key}: ${dataFile2[key]}`);
     }
-    // Keys Same, Values Different
+
+    // Key in file1, not in file2 — DELETED
+    // if (_.has(dataFile1, key) && !_.has(dataFile2, key)) {
+    if (Object.hasOwn(dataFile1, key) && !Object.hasOwn(dataFile2, key)) {
+      tempResult.push(`  - ${key}: ${dataFile1[key]}`);
+    }
+
+    // Keys Same, Values Different — CHANGED
     // if (_.has(dataFile1, key) && _.has(dataFile2, key)) {
     if (Object.hasOwn(dataFile1, key) && Object.hasOwn(dataFile2, key)) {
       if (dataFile1[key] !== dataFile2[key]) {
@@ -45,33 +29,65 @@ const comparer = (dataFile1, dataFile2) => {
         tempResult.push(`  + ${key}: ${dataFile2[key]}`);
       }
     }
-    // Key in file1, not in file2
-    // if (_.has(dataFile1, key) && !_.has(dataFile2, key)) {
-    if (Object.hasOwn(dataFile1, key) && !Object.hasOwn(dataFile2, key)) {
-      tempResult.push(`  - ${key}: ${dataFile1[key]}`);
-    }
 
-    // Key in file2, not in file1
-    // if (!_.has(dataFile1, key) && _.has(dataFile2, key)) {
-    if (!Object.hasOwn(dataFile1, key) && Object.hasOwn(dataFile2, key)) {
-      tempResult.push(`  + ${key}: ${dataFile2[key]}`);
+    // Keys Same, Values Same — UNCHANGED
+    // if (_.has(dataFile1, key) && _.has(dataFile2, key)) {
+    if (Object.hasOwn(dataFile1, key) && Object.hasOwn(dataFile2, key)) {
+      if (dataFile1[key] === dataFile2[key]) {
+        tempResult.push(`    ${key}: ${dataFile1[key]}`);
+      }
     }
 
     return tempResult;
   });
 
-  /* output v1
-  const resultConcat = _.concat('{', getDiff, '}');
-  const result = _.join(resultConcat, '\n');
-  return result;
-  */
+  // Output v1
+  // const resultConcat = _.concat('{', getDiff, '}');
+  // const result = _.join(resultConcat, '\n');
+  // return result;
 
-  /* output v2
-  */
+  // Output v2
   const result = `{\n${getDiff.join('\n')}\n}`;
   return result;
 
+  // Output v3
   // return `{\n${getDiff.join('\n')}\n}`;
 };
 
 export default comparer;
+
+/* v2
+const comparer = (dataFile1, dataFile2) => {
+  const keys = _.union(Object.keys(dataFile1), Object.keys(dataFile2)).sort();
+  // const keys = _.union(_.keys(dataFile1), _.keys(dataFile2)).sort();
+
+  const getDiff = keys.map((key) => {
+    // Key in file2, not in file1 — ADDED
+    if (!Object.hasOwn(dataFile1, key)) {
+      return `  + ${key}: ${dataFile2[key]}`;
+    }
+
+    // Key in file1, not in file2 — DELETED
+    if (!Object.hasOwn(dataFile2, key)) {
+      return `  - ${key}: ${dataFile1[key]}`;
+    }
+
+    // Keys Same, Values Different — CHANGED
+    if (!_.isEqual(dataFile1[key], dataFile2[key])) {
+      return `  - ${key}: ${dataFile1[key]}\n  + ${key}: ${dataFile2[key]}`;
+    }
+
+    // Keys Same, Values Same — UNCHANGED
+    return `    ${key}: ${dataFile1[key]}`;
+  });
+
+  // Output v1
+  // const resultConcat = _.concat('{', getDiff, '}');
+  // const result = _.join(resultConcat, '\n');
+  // return result;
+
+  // Output v2
+  const result = `{\n${getDiff.join('\n')}\n}`;
+  return result;
+};
+*/
