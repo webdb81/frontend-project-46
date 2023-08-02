0da822339b9cbfc4af24ffc1d7cacbe7af63bd1c
webdb81 <webdb81@gmail.com>
2023-08-02 10:46:49 +0300

    Testing fix json to end

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -15,7 +15,7 @@ test.each(fileFormat)('compare %p files', (format) => {
   const file1 = getFilePath(`file1.${format}`);
   const file2 = getFilePath(`file2.${format}`);
 
-  expect(gendiff(file1, file2, 'json')).toEqual(getExpectedResult('expected_result_json.txt'));
   expect(gendiff(file1, file2, 'stylish')).toEqual(getExpectedResult('expected_result_stylish.txt'));
   expect(gendiff(file1, file2, 'plain')).toEqual(getExpectedResult('expected_result_plain.txt'));
+  expect(gendiff(file1, file2, 'json')).toEqual(getExpectedResult('expected_result_json.txt'));
 });
